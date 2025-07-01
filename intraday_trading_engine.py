c197a83f0a83de16bd4598cb9edfbe9d18342953
mitin111 <mitin.aglawe111@gmail.com>
2025-07-01 19:21:29 +0530

    Add files via upload
    Add intraday_trading_engine.py

---
+++ intraday_trading_engine.py
@@ -0,0 +1,8 @@
+# intraday_trading_engine.py
+
+class TradingEngine:
+    def __init__(self):
+        print("TradingEngine initialized")
+
+    def get_signal(self):
+        return "Buy or Sell signal logic here"
