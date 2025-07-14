ddbfca34c74a425ff037f6b80dacbb1b36c95236
mitin111 <mitin.aglawe111@gmail.com>
2025-07-14 09:43:21 +0530

    Update stock_dashboard_phase1.py

--- stock_dashboard_phase1.py
+++ stock_dashboard_phase1.py
@@ -6,7 +6,8 @@ import os
 
 # Load credentials from .env
 load_dotenv()
-DEFAULT_BASE_URL = os.getenv("PROSTOCKS_BASE_URL", "https://api.prostocks.com")
+DEFAULT_BASE_URL = os.getenv("PROSTOCKS_BASE_URL", "https://starapiuat.prostocks.com")
+
 
 DEFAULT_UID = os.getenv("PROSTOCKS_USER_ID", "")
 DEFAULT_PWD = os.getenv("PROSTOCKS_PASSWORD", "")
