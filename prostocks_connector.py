e83c82ee5c74730b4054aeff7c253b18bb229f48
mitin111 <mitin.aglawe111@gmail.com>
2025-08-27 10:37:55 +0530

    Update prostocks_connector.py

--- prostocks_connector.py
+++ prostocks_connector.py
@@ -465,7 +465,7 @@ class ProStocksAPI:
         except Exception as e:
             print("❌ stop_ticks error:", e)
 
-     def connect_websocket(self, symbols, on_tick=None, tick_file="ticks.log"):
+    def connect_websocket(self, symbols, on_tick=None, tick_file="ticks.log"):
         """
         Wrapper so that dashboard call works.
         Internally uses start_ticks.
@@ -473,4 +473,3 @@ class ProStocksAPI:
         # on_tick callback store kar lo (agar diya gaya hai)
         self._on_tick = on_tick
         return self.start_ticks(symbols, tick_file=tick_file)
-
