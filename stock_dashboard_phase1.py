fddcb7555ca82fbf3add00ef0621cf8ffa035df4
mitin111 <mitin.aglawe111@gmail.com>
2025-09-07 13:00:17 +0530

    Update stock_dashboard_phase1.py

--- stock_dashboard_phase1.py
+++ stock_dashboard_phase1.py
@@ -286,7 +286,7 @@ with tab5:
             rangeslider_visible=False,
             rangebreaks=[
                 dict(bounds=["sat", "mon"]),
-                dict(bounds=[15.5, 9.25], pattern="hour")
+                dict(bounds=[15.5, 9.25], pattern="hour"),
                 dict(values=full_holidays)
             ]
         )
@@ -491,4 +491,5 @@ with tab5:
 
     if processed == 0 and ui_queue.qsize() == 0 and (not st.session_state.ohlc_x):
         placeholder_ticks.info("⏳ Waiting for first ticks...")
+
 
