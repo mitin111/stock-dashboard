a6b938f47f63731aa9963c9d597dc98ccbe55f72
mitin111 <mitin.aglawe111@gmail.com>
2025-11-19 12:47:13 +0530

    Update stock_dashboard_phase1.py

--- stock_dashboard_phase1.py
+++ stock_dashboard_phase1.py
@@ -267,9 +267,8 @@ with tab3:
 
 # === Tab 4: Indicator Settings === 
 with tab4:
-    from tab4_auto_trader import render_tab4
-    from tkp_trm_chart import ensure_trm_settings_loaded, render_trm_settings_ui_body
     from tab4_auto_trader import render_tab4, init_backend_session
+    from tkp_trm_chart import ensure_trm_settings_loaded, render_trm_settings_ui_body
 
     st.subheader("📀 Indicator & TRM Settings")
 
@@ -1033,6 +1032,7 @@ with tab5:
 
         else:
             st.warning(" Need at least 50 candles for TRM indicators.\nIncrease TPSeries max_days or choose larger interval.")
+
 
 
 
